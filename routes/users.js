5c80b28901fed4fd808d0653680a7316afef63ec
Mohit Yadav <mohitsryadav@gmail.com>
2020-09-23 11:58:54 +0530

    added /user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Usercontroller = require("../controllers/users");
-router.get("/getUsers", Usercontroller.getUsers);
-router.post("/createUSer", Usercontroller.createUser);
+router.get("/user/getUsers", Usercontroller.getUsers);
+router.post("/user/createUser", Usercontroller.createUser);
 router.post("/user/login", Usercontroller.userLogin);
 router.get("/user/get", Usercontroller.getId);
 router.put("/user/delete", Usercontroller.deleteUser);
-router.get("/users/list/:page", Usercontroller.getPages);
+router.get("/user/list/:page", Usercontroller.getPages);
 module.exports = router;
